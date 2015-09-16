b8bd1b8fa55fd6b32384368c8d34273253711df0
g.marco.dome@gmail.com <g.marco.dome@gmail.com>
2015-09-16 17:55:18 +0200

    distanza->distanza

--- src/tracking/tracking_09.py
+++ src/tracking/tracking_09.py
@@ -15,7 +15,7 @@ N_ITER = 5
 
 EXT = ".csv"
 
-def distance(p1,p2):
+def distanza(p1,p2):
 		
 	#calcola la distanza geometrica tra due punti in un frame		
 	dis = (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2
