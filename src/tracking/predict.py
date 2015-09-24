88e3fcbb8b3c4874c540e07e707bf4bb7f1d3379
dado90 <davide.centioni90@gmail.com>
2015-09-24 17:41:55 +0200

    Delete predict.py

--- src/tracking/predict.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-
-svm = cv2.SVM()
-
-svm.load("training.xml")
-z = np.array([1903.0,215.898617377,7689.5,366.350285411,40.0,24.6274166107,1444,459.0,1113.255,255,255,255,0,0,0],dtype = np.float32)
-
-print svm.predict(z)
